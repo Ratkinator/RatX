cdde314ee4fcf8831fee1d3816cd6a7bc9a77e94
Ratkinator <vvladut245@gmail.com>
2026-01-01 21:43:41 +0200

    Update Pixel Blade.lua

--- Pixel Blade.lua
+++ Pixel Blade.lua
@@ -1,2 +1,1 @@
-local u = "aHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L3FGQXlWTXZV"
-loadstring(game:HttpGet(game:GetService("HttpService"):Base64Decode(u)))()
+loadstring(game:HttpGet("https://pastebin.com/raw/qFAyVMvU"))()
