5dc0badd93bb53084dfb72aae4de6f7d0ef74c4f
R H <165597973+rhuda21@users.noreply.github.com>
2026-01-03 17:01:32 +0000

    Update Executed.lua

--- Executed.lua
+++ Executed.lua
@@ -1,25 +1,27 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Loader.lua"))()
-local function getGameName(id)
+local function getGameName(Games, id)
     local url = Games[id]
     if not url then return "Universal Script" end
     local filename = url:match("([^/]+)%.lua$"):gsub("%%20", " ")
     return filename:gsub("%.lua$", ""):gsub("%.Lua$", "")
 end
-local HttpService = game:GetService("HttpService")
-local function incrementExecutionCount()
-    local success, response = pcall(function()
-        local gameName = getGameName(game.GameId)
-        local res = request({
-            Url = "https://halal-worker.vvladut245.workers.dev/",
-            Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json",
-                ["GameName"] = gameName
-            },
-            Body = "{}"
-        })
-        return res and res.Body and HttpService:JSONDecode(res.Body)
-    end)
-    return (success and response and response.global_count) or "Unknown"
+return function(Games)
+    local HttpService = game:GetService("HttpService")
+    local function incrementExecutionCount()
+        local success, response = pcall(function()
+            local gameName = getGameName(Games, game.GameId)
+            local res = request({
+                Url = "https://halal-worker.vvladut245.workers.dev/",
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json",
+                    ["GameName"] = gameName
+                },
+                Body = "{}"
+            })
+            return res and res.Body and HttpService:JSONDecode(res.Body)
+        end)
+        return (success and response and response.global_count) or "Unknown"
+    end
+    local executionCount = incrementExecutionCount()
+    return executionCount
 end
-executionCount = incrementExecutionCount()
