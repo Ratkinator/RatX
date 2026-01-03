05791f4a4a61ed7c989d64afd074be597d1bc5aa
Ratkinator <vvladut245@gmail.com>
2026-01-03 14:59:45 +0200

    Update Executed.lua

--- Executed.lua
+++ Executed.lua
@@ -1,1 +1,25 @@
-
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Loader.lua"))()
+local function getGameName(id)
+    local url = Games[id]
+    if not url then return "Universal Script" end
+    local filename = url:match("([^/]+)%.lua$"):gsub("%%20", " ")
+    return filename:gsub("%.lua$", ""):gsub("%.Lua$", "")
+end
+local HttpService = game:GetService("HttpService")
+local function incrementExecutionCount()
+    local success, response = pcall(function()
+        local gameName = getGameName(game.GameId)
+        local res = request({
+            Url = "worker url",
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["GameName"] = gameName
+            },
+            Body = "{}"
+        })
+        return res and res.Body and HttpService:JSONDecode(res.Body)
+    end)
+    return (success and response and response.global_count) or "Unknown"
+end
+executionCount = incrementExecutionCount()
