357653c466cb35df5a715626b350f989abf390ec
Ratkinator <vvladut245@gmail.com>
2026-01-06 09:56:56 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 local Games = {
     [6161049307] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Pixel%20Blade.lua",
+    [9266873836] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/AFSE.lua",
 }
 local scriptUrl = Games[game.GameId]
 if scriptUrl then
