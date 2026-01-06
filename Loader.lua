38a24c9429a3e6e94a9f49297d3e2e7206379390
R H <165597973+rhuda21@users.noreply.github.com>
2026-01-06 20:37:22 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,8 @@
-local Games = {
-    [6161049307] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Pixel%20Blade.lua",
-    [9266873836] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/AFSE.lua",
-}
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Games.lua"))()
 local scriptUrl = Games[game.GameId]
 if scriptUrl then
     loadstring(game:HttpGet(scriptUrl))()
-    Tracker = game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Executed.lua")
-    local Run = loadstring(Tracker)
-    task.spawn(function() Run(Games) end)
+    task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Executed.lua"))() end)
 else
     warn("❌ Unsupported game | GameId:", game.GameId)
 end
