643bf32304c64771ebecb422c9de5267e57c399e
R H <165597973+rhuda21@users.noreply.github.com>
2026-01-06 20:40:00 +0000

    Update Executed.lua

--- Executed.lua
+++ Executed.lua
@@ -1,27 +1,22 @@
-local function getGameName(Games, id)
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Games.lua"))()
+local function getGameName(id)
     local url = Games[id]
     if not url then return "Universal Script" end
     local filename = url:match("([^/]+)%.lua$"):gsub("%%20", " ")
     return filename:gsub("%.lua$", ""):gsub("%.Lua$", "")
 end
-return function(Games)
-    local HttpService = game:GetService("HttpService")
-    local function incrementExecutionCount()
-        local success, response = pcall(function()
-            local gameName = getGameName(Games, game.GameId)
-            local res = request({
-                Url = "https://halal-worker.vvladut245.workers.dev/",
-                Method = "POST",
-                Headers = {
-                    ["Content-Type"] = "application/json",
-                    ["GameName"] = gameName
-                },
-                Body = "{}"
-            })
-            return res and res.Body and HttpService:JSONDecode(res.Body)
-        end)
-        return (success and response and response.global_count) or "Unknown"
-    end
-    local executionCount = incrementExecutionCount()
-    return executionCount
-end
+local HttpService = game:GetService("HttpService")
+local success, response = pcall(function()
+local gameName = getGameName(game.GameId)
+local res = request({
+Url = "https://halal-worker.vvladut245.workers.dev/",
+Method = "POST",
+Headers = {
+["Content-Type"] = "application/json",
+["GameName"] = gameName
+},
+Body = "{}"
+})
+return res and res.Body and HttpService:JSONDecode(res.Body)
+end)
+return (success and response and response.global_count) or "Unknown"
