6fb1381c712676f2bd41691fef6a79d4681f307f
R H <165597973+rhuda21@users.noreply.github.com>
2026-01-06 20:35:42 +0000

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,4 @@
+return {
+    [6161049307] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Pixel%20Blade.lua",
+    [9266873836] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/AFSE.lua",
+}
