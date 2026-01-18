438836d7727e1f79e2c85e511b9156d451141bf9
Ratkinator <vvladut245@gmail.com>
2026-01-18 22:08:53 +0200

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,5 @@
 return {
-    [6161049307] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Pixel%20Blade.lua",
+    [18172550962] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Pixel%20Blade.lua",
     [9266873836] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/AFSE.lua",
     [9363735110] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20Tsunami%20for%20Brainrot.lua",
     [121230136531797] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20logs%20for%20Brainrot.lua",
