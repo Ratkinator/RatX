e062729c0b868ba6752f8e31cf5116215d02c233
Ratkinator <vvladut245@gmail.com>
2026-02-07 12:04:56 +0200

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -5,4 +5,5 @@ return {
     [9497625867] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20logs%20for%20Brainrot.lua",
     [8967864843] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/You%20vs%20Homer.lua",
     [9344307274] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Break%20a%20Lucky%20Block.lua",
+    [9527851429] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Save%20brainrots%20from%20lava.lua",
 }
